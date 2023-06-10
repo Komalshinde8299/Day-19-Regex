b5493544f3ab410ab36c28f91ec02c80bd37f0b8
Komal Shinde <komalshinde8299@gmail.com>
2023-06-10 12:00:25 +0530

    Added:Code for Entring valid first name is addeed

---
+++ src/com/bridgeLabz/day19/regex/RegexProblems.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.day19.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexProblems {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("^[A-Z][a-z]{3,}" );
+        Matcher m = p.matcher("Komal");
+        boolean result = m.matches();
+        System.out.println(result);
+    }
+}
